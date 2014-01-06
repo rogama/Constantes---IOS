9603132431763c3f118e5e6ad9924f61f015daf8
rober <gascon87@gmail.com>
2014-01-06 19:54:15 +0100

    Initial Commit

---
+++ constantes/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  constantes
+//
+//  Created by rober on 06/01/14.
+//  Copyright (c) 2014 rogama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ constantes/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  constantes
+//
+//  Created by rober on 06/01/14.
+//  Copyright (c) 2014 rogama. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
